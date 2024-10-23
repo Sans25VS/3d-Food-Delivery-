1d6633ef367ec6a5e703aabf32cbc49e66d995a0
sanskar <sanskargupta37081@gmail.com>
2024-10-23 11:04:07 +0530

    dependency added expogl

--- 3d-burger-app/app/index.tsx
+++ 3d-burger-app/app/index.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Canvas } from "@react-three/fiber/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the 3D Burger App!
-      </Text>
+      <Canvas camera={{ position: [0, 0, 10] }}>
+        <color attach="background" args={["#512DAB"]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
+      <Text>Welcome to the 3D Burger App!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +24,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
