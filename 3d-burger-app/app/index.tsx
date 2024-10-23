1abab3f0c42fb2be5e16fca069d28e2832131331
sanskar <sanskargupta37081@gmail.com>
2024-10-23 11:18:01 +0530

    imported components

--- 3d-burger-app/app/index.tsx
+++ 3d-burger-app/app/index.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { Canvas } from "@react-three/fiber/native";
-import BottomUI from "../components/BottomUI";
+import { StatusBar } from "expo-status-bar";
+import { Suspense } from "react";
+import { StyleSheet, View } from "react-native";
+import { BottomUI } from "../components/BottomUI";
+import { Sandwich } from "../components/Sandwich";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Canvas camera={{ position: [0, 0, 10] }}>
-        <color attach="background" args={["#512DAB"]} />
-        <ambientLight intensity={0.5} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial color="hotpink" />
-        </mesh>
+      <Canvas camera={{ position: [-2, 2.5, 5], fov: 30 }}>
+        <color attach="background" args={["#512DA8"]} />
+        <Suspense>
+          <Sandwich />
+        </Suspense>
       </Canvas>
       <BottomUI />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -22,6 +23,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
